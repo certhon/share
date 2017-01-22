09845df68fdffedc1543fd1b1977fc10a7abc7ab
chenhong <yidong-Android6@yhd.com>
2017-01-22 16:05:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yidong_android6/demo/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.yidong_android6.demo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button text = (Button) findViewById(R.id.heheda);
+        Button image = (Button) findViewById(R.id.memeda);
+        text.setOnClickListener(this);
+     //   image.setOnClickListener(this);
+    }
+        //分享文字
+
+    public void shareText(View view) {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "This is my Share text.");
+        shareIntent.setType("text/plain");
+
+        //设置分享列表的标题，并且每次都显示分享列表
+        startActivity(Intent.createChooser(shareIntent, "分享到"));
+    }
+
+    //分享单张图片
+    public void shareSingleImage(View view) {
+        String imagePath = Environment.getExternalStorageDirectory() + File.separator + "test.jpg";
+        //由文件得到uri
+        Uri imageUri = Uri.fromFile(new File(imagePath));
+        Log.d("share", "uri:" + imageUri);  //输出：file:///storage/emulated/0/test.jpg
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+        shareIntent.setType("image/*");
+        startActivity(Intent.createChooser(shareIntent, "分享到"));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.heheda:
+                shareText(v);
+                break;
+            case R.id.memeda:
+                shareSingleImage(v);
+                break;
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/yidong_android6/demo/ShareActivity.java
@@ -0,0 +1,7 @@
+package com.example.yidong_android6.demo;
+
+/**
+ * Created by yidong-Android6 on 2017/1/17.
+ */
+public class ShareActivity {
+}
